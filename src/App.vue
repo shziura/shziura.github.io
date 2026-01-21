4a0a7ef6891615bdf678bd37e5113260285a1133
shizu <lmmitsch2706@gmail.com>
2026-01-21 13:51:10 +0100

    switch to typescript

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script lang="ts" setup>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-neutral-900 text-white">
+    <h1 class="text-4xl font-bold mb-4">This is a WIP</h1>
+  </div>
+</template>
+
